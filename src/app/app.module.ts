71e8222f3062946f74cdaa27629258bc839ea88e
unknown <joydeep127@gmail.com>
2019-12-24 20:21:52 +0530

    Q8-9 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { Component1Component } from './component1/component1.component';
 import { Component2Component } from './component2/component2.component';
 import { Service1Service } from './service1.service';
 import { Service2Service } from './service2.service';
+import { Component3Component } from './component3/component3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
-    Component2Component
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule,
